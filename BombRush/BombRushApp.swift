0b6103d7f299d7db53ba573be4873cf0d732e657
Cesar Venzor <cesarvenzor@protonmail.com>
2025-10-21 15:37:54 -0600

    Initial Commit

---
+++ BombRush/BombRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombRushApp.swift
+//  BombRush
+//
+//  Created by César Venzor on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BombRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
